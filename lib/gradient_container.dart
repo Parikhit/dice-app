be9853938d256d286b50e8ce8dbf1a0c66a7f75c
Parikhit <parikhitritzz@gmail.com>
2024-02-14 17:37:44 +0530

    Adding Custom Widgets

---
+++ lib/gradient_container.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+//Using positional arguments that directly takes colors which then is added to the List.
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText('Hello World!'),
+      ),
+    );
+  }
+}
+
+
+//Using named argument that takes gradient colors as List of Colors
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Hello World!'),
+//       ),
+//     );
+//   }
+// }
