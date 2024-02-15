3acd6599c9de2d36a0b4a337574840585b7c18be
Parikhit <parikhitritzz@gmail.com>
2024-02-15 17:11:45 +0530

    Adding stateful widget for Dice Roll

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
+import 'package:first_app/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -23,7 +23,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: StyledText('Hello World!'),
+        child: DiceRoller(),
       ),
     );
   }
